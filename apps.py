3f1cf3c97408730bec2a78d12b1c1c42f5bb637d
Shanoon Issaka <shanoongaspi@gmail.com>
2019-07-18 14:43:55 +0100

    modified apps.py

--- apps.py
+++ apps.py
@@ -1,4 +1,29 @@
-from flask import Flask, render_template
+from flask import *
+import json
+import os
+app = Flask(__name__)
+items = []
+@app.route('/')
+def index():
+    return render_template('form2.html', items=items)
 
+@app.route('/add_todo')
+def add_todo():
+    item = request.args.get('item')
+    print(item)
+    items.append(item)
+    return redirect("/", code=302)
+
+@app.route('/get_todos')
+def get_todos():
+    resp = Response(json.dumps(items))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+ 
+@app.route('/foo/<name>')
 def foo(name):
     return render_template('index.html', to=name)
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
